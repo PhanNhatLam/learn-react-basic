4bff14b48947ece5ad82cf08e775c35a5ea623d2
Lawm <lampn1998@gmail.com>
2023-09-17 10:55:23 +0700

    Demo Forms in ReactJS

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -3,28 +3,48 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
-        name: 'Lam',
-        age: 24
+        fname: "",
+        lname: ""
     }
 
-    handleOnChangeName = (event) => {
+    handleChangeFirstName = (event) => {
         this.setState({
-            name: event.target.value
+            fname: event.target.value
         })
+    }
+
+    handleChangeLastName = (event) => {
+        this.setState({
+            lname: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('>>>check data:', this.state);
     }
 
     render() {
 
         return (
             <>
-                <div className="first">
-                    <input value={this.state.name} type="text"
-                        onChange={(event) => this.handleOnChangeName(event)} />
-                    Hello, my name is {this.state.name}
-                </div>
-                <div className="second">
-                    My age is {this.state['age']}
-                </div>
+                <form action="/action_page.php">
+                    <label htmlFor="fname">First name:</label>
+                    <br />
+                    <input type="text" value={this.state.fname}
+                        onChange={(event) => this.handleChangeFirstName(event)}
+                    />
+                    <br />
+                    <label htmlFor="lname">Last name:</label>
+                    <br />
+                    <input type="text" value={this.state.lname}
+                        onChange={(event) => this.handleChangeLastName(event)}
+                    />
+                    <br /><br />
+                    <input type="button" value="Submit"
+                        onClick={(event) => this.handleSubmit(event)}
+                    />
+                </form>
             </>
         )
     }
